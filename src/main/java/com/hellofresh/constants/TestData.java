1e981eb955bbd970856aa16e4e750fcaee42a271
Hassan Radi <hassan.muhammad1990@gmail.com>
2018-06-28 00:01:14 +0200

    Initial code classes

---
+++ src/main/java/com/hellofresh/constants/TestData.java
@@ -0,0 +1,9 @@
+package com.hellofresh.constants;
+
+/**
+ * Class to act as a container for all the test data in the project. When you need to change any of
+ * these values, you change it in one location and it reflects everywhere else in the whole project.
+ */
+public class TestData {
+
+}
